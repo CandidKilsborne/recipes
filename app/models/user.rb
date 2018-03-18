793961b06c8eef61b89f604310bf2de3acb01c20
CandidKilsborne <a.paul.piotrowski@gmail.com>
2018-03-18 13:37:44 -0500

    add email format test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false} 
 end
