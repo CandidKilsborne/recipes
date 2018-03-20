5ad7abbd170e2d8a797adf2d0817030591422daa
CandidKilsborne <a.paul.piotrowski@gmail.com>
2018-03-19 20:18:28 -0500

    user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310011535) do
+ActiveRecord::Schema.define(version: 20180320010353) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
